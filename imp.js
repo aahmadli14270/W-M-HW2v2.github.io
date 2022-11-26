96108647c4d7cb60d0099cb9ee75dc28fcb032c5
aahmadli14270 <84677778+aahmadli14270@users.noreply.github.com>
2022-11-26 16:31:31 +0400

    OpenWeater fetch data is already online in console

--- imp.js
+++ imp.js
@@ -8,18 +8,18 @@ navigator.geolocation.getCurrentPosition(
 
     const map = L.map("map").setView([latitude, longitude], 6);
 
-    L.tileLayer(
-      "http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga",
-      {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }
-    ).addTo(map);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
     L.marker([latitude, longitude])
       .addTo(map)
       .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
       .openPopup();
+
+    console.log(customerCall(latitude, longitude));
+    // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
   },
   function () {
     alert(
@@ -27,3 +27,24 @@ navigator.geolocation.getCurrentPosition(
     );
   }
 );
+
+function customerCall(latitude, longitude) {
+  let lat = latitude;
+  let lon = longitude;
+  let units = "metric";
+  let lang = "en";
+  let key = "5209b666803238e6492b0f84bb620f41";
+  // let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
+  //let url = `https://api.openweathermap.org/data/2.5/onecall?lat=40.405520074042514&lon=49.844963102044325&appid=51a6bf5e7efb2ebbc1b35e2519d42ff1&units=metric&lang=en`;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`
+  )
+    .then((response) => {
+      if (!response.ok) throw new Error(response.statusText);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(console.err);
+}
