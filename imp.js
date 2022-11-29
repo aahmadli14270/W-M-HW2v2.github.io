177f056eb83982efc4516ba5616735a3ddb51305
aahmadli14270 <84677778+aahmadli14270@users.noreply.github.com>
2022-11-29 20:38:53 +0400

    initial version of hero-section button-controlled

--- imp.js
+++ imp.js
@@ -12,6 +12,8 @@ tomorrowplusplus.setDate(temp.getDate() + 3);
 
 const searchbar = document.querySelector(".search-bar");
 const searchbutton = document.querySelector(".search");
+const herosearch = document.querySelector(".hero-current");
+
 let searchforAPI = "";
 
 searchbutton.addEventListener("submit", (e) => {});
@@ -70,69 +72,70 @@ const dayNames = [
 
 console.log(date3D);
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    const flexColumn = document.querySelector(".flex-columns-container");
+herosearch.addEventListener("click", (e) => {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const flexColumn = document.querySelector(".flex-columns-container");
 
-    flexColumn.classList.add("flex-columns-container-addup");
-    flexColumn.innerHTML = `<div class="user-columns border-right main-weather-details"></div>
+      flexColumn.classList.add("flex-columns-container-addup");
+      flexColumn.innerHTML = `<div class="user-columns border-right main-weather-details"></div>
           <div
             class="user-columns border-right secondary-weather-details"
           ></div>
           <div class="user-columns" id="map"></div>
         </div>`;
 
-    // console.log(position);
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(latitude, longitude);
+      // console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
 
-    const map = L.map("map").setView([latitude, longitude], 6);
+      const map = L.map("map").setView([latitude, longitude], 6);
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-    L.marker([latitude, longitude])
-      .addTo(map)
-      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-      .openPopup();
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
 
-    console.log(openWeatherCall(latitude, longitude));
-    // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-  },
-  function () {
-    alert(
-      "Couldn't get your current location... Please give location permission."
-    );
+      console.log(openWeatherCall(latitude, longitude));
+      // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+    },
+    function () {
+      alert(
+        "Couldn't get your current location... Please give location permission."
+      );
+    }
+  );
+
+  function openWeatherCall(latitude, longitude) {
+    let lat = latitude;
+    let lon = longitude;
+    let units = "metric";
+    let lang = "en";
+    let key = "5209b666803238e6492b0f84bb620f41";
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
+    //let url = `https://api.openweathermap.org/data/2.5/onecall?lat=40.405520074042514&lon=49.844963102044325&appid=51a6bf5e7efb2ebbc1b35e2519d42ff1&units=metric&lang=en`;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        weatherGenerator(data);
+      })
+      .catch(console.err);
   }
-);
 
-function openWeatherCall(latitude, longitude) {
-  let lat = latitude;
-  let lon = longitude;
-  let units = "metric";
-  let lang = "en";
-  let key = "5209b666803238e6492b0f84bb620f41";
-  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
-  //let url = `https://api.openweathermap.org/data/2.5/onecall?lat=40.405520074042514&lon=49.844963102044325&appid=51a6bf5e7efb2ebbc1b35e2519d42ff1&units=metric&lang=en`;
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) throw new Error(response.statusText);
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      weatherGenerator(data);
-    })
-    .catch(console.err);
-}
+  function weatherGenerator(data) {
+    const mainDetails = document.querySelector(".main-weather-details");
 
-function weatherGenerator(data) {
-  const mainDetails = document.querySelector(".main-weather-details");
-
-  let templateHTML = `
+    let templateHTML = `
   <img class = "cloud-img" src = "http://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png" alt = "${data.current.weather[0].description}">
     <p class = "property">humidity: ${data.current.humidity}</p>
     <p class = "property">pressure: ${data.current.pressure}</p>
@@ -140,10 +143,12 @@ function weatherGenerator(data) {
     <p class = "property">timezone: ${data.timezone}</p>
     `;
 
-  mainDetails.innerHTML = templateHTML;
-  const secondaryDetails = document.querySelector(".secondary-weather-details");
+    mainDetails.innerHTML = templateHTML;
+    const secondaryDetails = document.querySelector(
+      ".secondary-weather-details"
+    );
 
-  let templateHTMLsecondary = `
+    let templateHTMLsecondary = `
 
             <div class="day">
               <h1 class="date">${
@@ -173,8 +178,8 @@ function weatherGenerator(data) {
               <img class = "cloud-img" src = "http://openweathermap.org/img/wn/${
                 data.daily[tomorrow.getDay()].weather[0].icon
               }@4x.png" alt = "${
-    data.daily[tomorrow.getDay()].weather[0].description
-  }"/>
+      data.daily[tomorrow.getDay()].weather[0].description
+    }"/>
   <p class="temp">${Math.round(
     data.daily[tomorrow.getDay()].temp.day
   )} / ${Math.round(data.daily[tomorrow.getDay()].temp.night)} °C</p>
@@ -209,13 +214,13 @@ function weatherGenerator(data) {
               <img class = "cloud-img" src = "http://openweathermap.org/img/wn/${
                 data.daily[tomorrowplus.getDay()].weather[0].icon
               }@4x.png" alt = "${
-    data.daily[tomorrowplus.getDay()].weather[0].description
-  }"/>
+      data.daily[tomorrowplus.getDay()].weather[0].description
+    }"/>
                 <p class="temp">${Math.round(
                   data.daily[tomorrowplus.getDay()].temp.day
                 )} / ${Math.round(
-    data.daily[tomorrowplus.getDay()].temp.night
-  )} °C</p>
+      data.daily[tomorrowplus.getDay()].temp.night
+    )} °C</p>
               </div>
             </div>
 
@@ -247,8 +252,8 @@ function weatherGenerator(data) {
             <img class = "cloud-img" src = "http://openweathermap.org/img/wn/${
               data.daily[tomorrowplusplus.getDay()].weather[0].icon
             }@4x.png" alt = "${
-    data.daily[tomorrowplusplus.getDay()].weather[0].description
-  }"/>
+      data.daily[tomorrowplusplus.getDay()].weather[0].description
+    }"/>
   <p class="temp">${Math.round(
     data.daily[tomorrowplusplus.getDay()].temp.day
   )} / ${Math.round(data.daily[tomorrowplusplus.getDay()].temp.night)} °C</p>
@@ -256,8 +261,9 @@ function weatherGenerator(data) {
           </div>
 
   `;
-  secondaryDetails.innerHTML = templateHTMLsecondary;
-}
+    secondaryDetails.innerHTML = templateHTMLsecondary;
+  }
+});
 
 //
 //   let units1 = "metric";
